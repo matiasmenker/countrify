58c507c941f2164a8d406b9d91a94f171b718d2f
Matias Menker <menkermatias@gmail.com>
2021-11-21 21:23:52 +0000

    implementation data

---
+++ src/data/RestCountryData.ts
@@ -0,0 +1,107 @@
+import CountryRepository from 'core/repositories/CountryRepository';
+import Country from 'core/entities/Country';
+import Language from 'core/entities/Language';
+import axios, { AxiosError } from 'axios';
+import { ErrorResponseType, HttpResponse } from 'core/entities/Response';
+
+type CountryJson = {
+    name: {
+        common: string;
+        official: string;
+    };
+    tld: [];
+    cca2: string;
+    ccn3: string;
+    cca3: string;
+    cioc: string;
+    independent: boolean;
+    status: string;
+    unMember: boolean;
+    currencies: {};
+    idd: {};
+    capital: [string];
+    altSpellings: [];
+    region: string;
+    subregion: string;
+    languages: {
+        [key: string]: string;
+    };
+    translations: {};
+    latlng: [];
+    landlocked: true;
+    borders: [];
+    area: number;
+    demonyms: {};
+    flag: string;
+    maps: {
+        googleMaps: string;
+        openStreetMaps: string;
+    };
+    population: number;
+    fifa: string;
+    car: {};
+    timezones: [];
+    continents: [];
+    flags: {
+        png: string;
+        svg: string;
+    };
+    coatOfArms: {};
+    startOfWeek: string;
+    capitalInfo: {
+        latlng: number[];
+    };
+};
+
+const REST_COUNTRIES_URL = 'https://restcountries.com/v3.1';
+
+class RestCountryData implements CountryRepository {
+    public async all() {
+        try {
+            const { data, status, statusText } = await axios.get(`${REST_COUNTRIES_URL}/all`);
+            const countries = data.map(asCountry);
+            return new HttpResponse<Country[]>(status, statusText, countries);
+        } catch (error) {
+            const err = error as AxiosError;
+            if (err.response) {
+                return new HttpResponse<ErrorResponseType>(err.response.status, err.response.statusText);
+            } else {
+                return new HttpResponse<ErrorResponseType>(400, 'Bad Request');
+            }
+        }
+    }
+
+    public async getByCode(code: string) {
+        try {
+            const { data, status, statusText } = await axios.get(`${REST_COUNTRIES_URL}/alpha/${code}`);
+            const countries = asCountry(data[0]);
+            return new HttpResponse<Country>(status, statusText, countries);
+        } catch (error) {
+            const err = error as AxiosError;
+            if (err.response) {
+                return new HttpResponse<ErrorResponseType>(err.response.status, err.response.statusText);
+            } else {
+                return new HttpResponse<ErrorResponseType>(400, 'Bad Request');
+            }
+        }
+    }
+}
+
+const asCountry = (country: CountryJson) => {
+    return new Country(
+        country.name.common,
+        country.cca2,
+        country.region,
+        country.subregion,
+        country.flags.svg,
+        country.maps.googleMaps,
+        formatPopulation(country.population),
+        country.languages ? Object.values(country.languages).map((language: string) => new Language(language)) : null,
+        country.capital ? country.capital[0] : null,
+        null
+    );
+};
+
+const formatPopulation = (number: number) => number.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+
+export default RestCountryData;
---
+++ src/data/UnsplashData.ts
@@ -0,0 +1,68 @@
+import Image from 'core/entities/Image';
+import ImageRepository from 'core/repositories/ImageRepository';
+import { ErrorResponseType, HttpResponse } from 'core/entities/Response';
+import axios, { AxiosError } from 'axios';
+
+type ImageJson = {
+    total: number;
+    total_pages: number;
+    results: {
+        id: string;
+        created_at: string;
+        updated_at: string;
+        promoted_at: any;
+        width: number;
+        height: number;
+        color: string;
+        blur_hash: string;
+        description: any;
+        alt_description: string;
+        urls: {
+            full: string,
+            regular: string
+        };
+        links: {
+            self: string;
+            html: string;
+            download: string;
+            download_location: string;
+        };
+        categories: [];
+        likes: number;
+        liked_by_user: boolean;
+        current_user_collections: [];
+        sponsorship: boolean;
+        topic_submissions: {};
+        user: [];
+        tags: [];
+    }[];
+};
+
+const UNSPLASH_URL = 'https://api.unsplash.com';
+
+class UnsplashData implements ImageRepository {
+    public async search(name: string) {
+        try {
+            const { data, status, statusText } = await axios.get<ImageJson>(`${UNSPLASH_URL}/search/photos`, {
+                params: {
+                    query: name,
+                    client_id: process.env.UNSPLASH_TOKEN,
+                },
+            });
+            return new HttpResponse<Image>(status, statusText, new Image(
+                data.results[0].urls.full,
+                data.results[0].urls.regular,
+                data.results[0].alt_description
+            ));
+        } catch (error) {
+            const err = error as AxiosError;
+            if (err.response) {
+                return new HttpResponse<ErrorResponseType>(err.response.status, err.response.statusText);
+            } else {
+                return new HttpResponse<ErrorResponseType>(400, 'Bad Request');
+            }
+        }
+    }
+}
+
+export default UnsplashData;
