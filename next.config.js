16ffd1078a70d937c22f0f69fe160bdb0d983252
Matias Menker <menkermatias@gmail.com>
2021-11-23 16:53:32 +0000

    improve performance

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+    swcMinify: true,
     reactStrictMode: true,
     images: {
         domains: ['flagpedia.net', 'unsplash.com', 'images.unsplash.com'],
+    },
+    experimental: {
+        styledComponents: true,
     },
 };
